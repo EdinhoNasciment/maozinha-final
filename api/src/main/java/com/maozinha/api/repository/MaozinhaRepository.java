293883280119275643e8c97d316be6689bd90304
EdinhoNasciment <112394149+EdinhoNasciment@users.noreply.github.com>
2025-02-07 19:31:16 -0800

    Delete api directory

--- api/src/main/java/com/maozinha/api/repository/MaozinhaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maozinha.api.repository;
-
-import com.maozinha.api.domain.Maozinha;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaozinhaRepository extends JpaRepository<Maozinha, Long>{
-
-
-}
